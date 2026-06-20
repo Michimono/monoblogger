41c4e1cc86685bb15935cf2a302e6725162bb466
Eric <ericmourinmarin@gmail.com>
2026-06-21 01:00:45 +0200

    Renamed index.xml to index.html in @monoblogger/starter package
    Updated entry point in build.mjs and updated the build-xml.mjs plugin to inject xml header and generate correctly the final index.xml file

--- packages/core/src/build.mjs
+++ packages/core/src/build.mjs
@@ -5,7 +5,7 @@ import inlineCss from "./plugins/inline-css.mjs";
 
 export async function build() {
     const steps = [
-        () => buildXml("src/index.xml", "dist/index.xml"),
+        () => buildXml("src/index.html", "dist/index.xml"),
         () => buildCss("src/input.css", "dist/output.css"),
         () => inlineVars("dist/index.xml", "src/data.json"),
         () => inlineCss("dist/index.xml", "dist/output.css"),
